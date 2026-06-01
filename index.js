7199421f404c45cd9db0f8fd02631980531ea845
Abdul samad <abdulsmdpm786@gmail.com>
2026-06-01 11:06:56 +0530

    added frontend cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 // dotenv.config()
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "https://study-buddy-client-red.vercel.app"],
         credentials: true
     })
 )
