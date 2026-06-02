ded0ee375dc0a6200b82e8ea9c05db071a48ff65
Abdul samad <abdulsmdpm786@gmail.com>
2026-06-02 16:39:06 +0530

    otp issue

--- Utilities/sendEmail.js
+++ Utilities/sendEmail.js
@@ -5,9 +5,9 @@ export const sendEmail = async (option) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      host: process.env.EMAIL_HOST, 
+      port: parseInt(process.env.EMAIL_PORT) ||  587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
